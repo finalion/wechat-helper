e74f132c8157703bfbaffdcfccac266a9ec646f4
Liang Feng <Liang Feng>
2017-03-12 22:21:13 +0200

    add msg reply method when sleeping

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 #-*- coding:utf-8 -*-
 import itchat
-from itchat.content import TEXT
+from itchat.content import *
+import pytz
+import datetime
+
+
+def my_current_time(timezone='Europe/Helsinki'):
+    tz = pytz.timezone(timezone)
+    t = datetime.datetime.now(tz)
+    return t
+
+
+@itchat.msg_register
+def reply_msg_isat(msg):
+    my_time = my_current_time()
+    if 0 <= my_time.hour <= 23:
+        # itchat.send(u'我当前时间为凌晨%d点%d分。您的消息可能不会立即回复。（自动回复）' %
+        #             (my_time.hour, my_time.minute))
+        return u'我当前时间为凌晨%d点%d分。您的消息可能不会立即回复。（自动回复）' %
+            (my_time.hour, my_time.minute)
 
 
 @itchat.msg_register(TEXT,  isGroupChat=True)
