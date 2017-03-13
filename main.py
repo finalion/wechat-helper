81710b88392ed5c9ccabc8899425db29ed2e7a16
Liang Feng <Liang Feng>
2017-03-13 14:20:13 +0200

    update

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def my_current_time(timezone='Europe/Helsinki'):
     return t
 
 
-@itchat.msg_register([TEXT,VIDEO,RECORDING,PICTURE,ATTACHMENT], isGroupChat=False)
+@itchat.msg_register([TEXT, VIDEO, RECORDING, PICTURE, ATTACHMENT], isGroupChat=False)
 def reply_msg_untime(msg):
     my_time = my_current_time()
     if 0 <= my_time.hour <= 7:
@@ -24,14 +24,14 @@ def reply_msg_untime(msg):
 def reply_msg_isat(msg):
     if msg['isAt']:
         my_time = my_current_time()
+        itchat.send(u'%s 群聊 %s 中 %s 发来消息: %s' % (my_time.ctime(),
+                                                 msg['ActualUserName'], msg['ActualNickName'], msg['Content']))
         if 0 <= my_time.hour <= 7:
             # itchat.send(u'我当前时间为凌晨%d点%d分。您的消息可能不会立即回复。（自动回复）' %
             #             (my_time.hour, my_time.minute))
             return u'@%s 我当前时间为凌晨%d点%d分。您的消息可能不会立即回复。（自动回复）' % (msg['ActualNickName'], my_time.hour, my_time.minute)
-        
-        itchat.send(u'收到群聊 %s 中 %s 发来的消息: %s' %
-                    (msg['ActualUserName'], msg['ActualNickName'], msg['Content']))
-        return  u'@%s 您的消息已发送到我的邮箱和个人号，我会及时回复。（自动回复）' % (msg['ActualNickName'], msg['FromUserName'])
+
+        return u'@%s 您的消息已发送到我的邮箱和个人号，我会及时回复。（自动回复）' % (msg['ActualNickName'], msg['FromUserName'])
 
 
 def mail_content(msg):
