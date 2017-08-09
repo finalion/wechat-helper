5af657177850e49a6ef60bb365b4652561fc5881
vleon <finalion@gmail.com>
2017-08-10 00:21:49 +0300

    update

--- robot.py
+++ robot.py
@@ -1,16 +1,25 @@
 #-*- coding:utf-8 -*-
-import os
-import itchat
-from itchat.content import *
-import pytz
 import datetime
-import db
 import json
+import os
 import subprocess
+import time
 
+import itchat
+import pytz
+from itchat.content import *
+
+import db
+
+qr_path = "/tmp/QR.png"
+status_path = '/root/scripts/wechat-helper/itchat.pkl'
+
+ENABLE_SAVE_MSG = True
+IDENTITY = 'DigitalOcean'
 instance = itchat.new_instance()
 
 shell_mode = False
+
 
 def my_current_time(timezone='Europe/Helsinki'):
     tz = pytz.timezone(timezone)
@@ -19,16 +28,20 @@ def my_current_time(timezone='Europe/Helsinki'):
 
 
 def save_msg(msg):
-    to_store = msg['FromUserName'], msg.get('ActualNickName',None), msg['ToUserName'], msg['Text'] if msg[
-        'Type'] == 'Text' else msg['Type'], msg['Type'], msg['CreateTime']
+    if not db.connection:
+        return
+    text = msg['Text'] if msg['Type'] == 'Text' else msg['Type']
+    to_store = (msg['FromUserName'], msg.get('ActualNickName', None),
+                msg['ToUserName'], text, msg['Type'], msg['CreateTime'])
     db.insert(to_store)
+
 
 def is_me(sender_id):
     return instance.loginInfo['User']['UserName'] == sender_id
 
+
 @instance.msg_register([TEXT, VIDEO, RECORDING, PICTURE, ATTACHMENT], isGroupChat=False)
 def reply_msg_untime(msg):
-    print msg['FromUserName']
     if is_me(msg['FromUserName']):
         global shell_mode
         if msg['Text'].lower() == 'shell':
@@ -42,8 +55,9 @@ def reply_msg_untime(msg):
         output = run_cmd(msg['Text'])
         if output:
             return output
-   
-    if db.connection: save_msg(msg)
+
+    if ENABLE_SAVE_MSG:
+        save_msg(msg)
     my_time = my_current_time()
     if 0 <= my_time.hour < 7:
         # itchat.send(u'我当前时间为凌晨%d点%d分。您的消息可能不会立即回复。（自动回复）' %
@@ -53,11 +67,12 @@ def reply_msg_untime(msg):
 
 @instance.msg_register(TEXT,  isGroupChat=True)
 def reply_msg_isat(msg):
-    if db.connection: save_msg(msg)
+    if ENABLE_SAVE_MSG:
+        save_msg(msg)
     if msg['isAt']:
         my_time = my_current_time()
         instance.send(u'%s 群聊 %s 中 %s 发来消息: %s' % (my_time.ctime(),
-                                                 msg['ActualUserName'], msg['ActualNickName'], msg['Content']))
+                                                   msg['ActualUserName'], msg['ActualNickName'], msg['Content']))
         if 0 <= my_time.hour < 7:
             # itchat.send(u'我当前时间为凌晨%d点%d分。您的消息可能不会立即回复。（自动回复）' %
             #             (my_time.hour, my_time.minute))
@@ -65,17 +80,20 @@ def reply_msg_isat(msg):
 
         return u'@%s 您的消息已发送到我的邮箱和个人号，我会及时回复。（自动回复）' % msg['ActualNickName']
 
+
 def save_friends_data(update=False):
     friends = instance.get_friends(update=update)
-    with open('friends.json','wb') as f:
-        json.dump(friends,f)
+    with open('friends.json', 'wb') as f:
+        json.dump(friends, f)
     return friends
+
 
 def run_cmd(cmd):
     args = cmd.split(' ')
     args = [arg for arg in args if arg.strip()]
     if not args:
-        return 
+        return
+
     def _format(s):
         return s.split('\n')
     try:
@@ -83,8 +101,8 @@ def run_cmd(cmd):
         return output
     except OSError as e:
         # commands with no output
-#         print str(e)
-        if len(args)>1 and args[0]=='cd':
+        #         print str(e)
+        if len(args) > 1 and args[0] == 'cd':
             os.chdir(args[1])
             output = 'Switch directory: {}'.format(os.getcwd())
             return output
@@ -95,9 +113,26 @@ def run_cmd(cmd):
     except Exception as e:
         print str(e)
 
+
+def lc():
+    print('finish login')
+    instance.send(u'wechat exit at %s: %s' %
+                  (IDENTITY, time.ctime()), toUserName='filehelper')
+
+
+def ec():
+    print('exit')
+    instance.send(u'wechat exit at %s: %s' %
+                  (IDENTITY, time.ctime()), toUserName='filehelper')
+
 if __name__ == '__main__':
-    instance.auto_login(hotReload=True, #enableCmdQR=2,
-                  picDir="/tmp/QR.png",
-                  statusStorageDir='/root/scripts/wechat-helper/itchat.pkl')  # deploy on digital ocean
+    instance.auto_login(hotReload=True,  # enableCmdQR=2,
+                        picDir=qr_path,
+                        statusStorageDir=status_path,
+                        loginCallback=lc,
+                        exitCallback=ec)  # deploy on digital ocean
     save_friends_data()
+    # print type(instance.get_chatrooms())
+    # for chatroom in instance.get_chatrooms():
+    # print chatroom['NickName'].encode('gbk'), ',   ', chatroom['UserName']
     instance.run()
